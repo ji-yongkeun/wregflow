ab95715da424c98e7dcc071d05ae4502311e0912
ji-yongkeun <jyk1126@gmail.com>
2026-05-27 13:28:02 +0900

    8090 포트설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,              // 👈 [추가] 외부 IP(네이버 클라우드 공인 IP) 접속을 허용합니다!
     port: 8090,             // 로컬 개발 환경 포트 8090으로 설정
     strictPort: false,       // 포트 고정
     proxy: {
